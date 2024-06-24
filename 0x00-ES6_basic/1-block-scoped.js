2b099abe3ed5e4b6d0a121b628132e96a85059b2
sabimbi447 <asarejonas447@gmail.com>
2024-06-24 20:50:16 +0100

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true; // eslint-disable-line no-unused-vars
-      const task2 = false; // eslint-disable-line no-unused-vars
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
     }
-  
-    return [task, task2];
   }
+
+  return [task, task2];
+}
